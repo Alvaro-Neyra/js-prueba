ad3389e66655dd9e7784077d5793a4c7d47e48a2
Alvaro Alberto Neyra Salazar <alvaroprogramador.neyra.salazar@gmail.com>
2024-04-15 19:47:24 -0500

    Anadiendo ejercicio ages

---
+++ ages/js/index.js
@@ -0,0 +1,8 @@
+const edad = Number(prompt("Ingrese su edad:"));
+if (edad > 18) {
+    alert("Eres mayor de edad!");
+} else if (edad > 0){
+    alert("Eres menor de edad!");
+} else {
+    alert("Edad incorrecta!");
+}
