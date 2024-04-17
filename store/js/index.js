212319dec1b29563d65e6c0830e85ead3e5b6e84
Alvaro Alberto Neyra Salazar <alvaroprogramador.neyra.salazar@gmail.com>
2024-04-16 20:50:53 -0500

    Nuevos ejercicios

--- store/js/index.js
+++ store/js/index.js
@@ -1,10 +1,20 @@
-let totalAPagar = 0;
-let cantidad = 1;
-for (let i = 0; i <= cantidad; i++) {
-    const nombre = prompt("Que producto deseas llevar?");
-    const cantidad = Number(prompt("Cuantas unidades?"));
-    const precio = Number(prompt("Cuanto sale cada unidad?"));
-    const subTotal = cantidad * precio;
-    totalAPagar += subTotal;
+const comprar = () => {
+    let totalAPagar = 0;
+    for (let i = 1; i <= 3; i++) {
+        const nombre = prompt("Que producto deseas llevar?");
+        const cantidad = Number(prompt("Cuantas unidades?"));
+        const precio = Number(prompt("Cuanto sale cada unidad?"));
+        const subTotal = cantidad * precio;
+        totalAPagar = totalAPagar + subTotal;
+    }
+    console.log(totalAPagar);
+    return totalAPagar;
 }
-alert(`El total a pagar es: ${totalAPagar}`);
+
+const total1 = comprar();
+const total2 = comprar();
+const total3 = comprar();
+
+const total = total1 + total2 + total3;
+
+// const total = comprar() + comprar() + comprar();
